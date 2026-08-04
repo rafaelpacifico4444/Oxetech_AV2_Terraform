ee80bd82b5d9fe690363f5230ad43e49e3ef49b4
rafaelpacifico4444 <pacificolobo123@gmail.com>
2026-08-04 15:31:07 -0300

    acho que vai dar certo, mudei o iam-github... de main para master

--- terraform/iam-github-oidc.tf
+++ terraform/iam-github-oidc.tf
@@ -62,7 +62,7 @@ data "aws_iam_policy_document" "github_assume_role" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
-        "repo:rafaelpacifico4444/Oxetech_AV2_Terraform:ref:refs/heads/main",
+        "repo:rafaelpacifico4444/Oxetech_AV2_Terraform:ref:refs/heads/master",
         "repo:rafaelpacifico4444/Oxetech_AV2_Terraform:ref:refs/heads/develop",
         "repo:rafaelpacifico4444/Oxetech_AV2_Terraform:pull_request"
       ]
