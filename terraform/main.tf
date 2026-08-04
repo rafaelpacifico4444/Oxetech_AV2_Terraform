3f4e9f8e7b4571200768b8c2e6c4469f7c925962
rafaelpacifico4444 <pacificolobo123@gmail.com>
2026-08-04 10:51:13 -0300

    test: validate actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,3 +49,4 @@ data "aws_availability_zones" "available" {
 data "aws_ssm_parameter" "ubuntu_2404" {
   name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
 }
+# Test comment
